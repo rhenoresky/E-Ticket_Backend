522513b6bf58c53a7ad0bfd28b008d5e9d9839f0
rhenoresky <rheno2002@gmail.com>
2024-05-25 23:34:34 +0700

    add admin seed

---
+++ prisma/seed/createAdmin.js
@@ -0,0 +1,16 @@
+import { prisma } from "../../src/db/prisma";
+import { generateId } from "../../src/tools/generateId";
+import { passwordHashing } from "../../src/tools/hash";
+
+const createAccount = async (password, id) => {
+  await prisma.account.create({
+    data: {
+      id,
+      email: "admin@gmail.com",
+      password,
+      role: "ADMIN",
+    },
+  });
+};
+
+await createAccount(await passwordHashing("admin"), generateId());
