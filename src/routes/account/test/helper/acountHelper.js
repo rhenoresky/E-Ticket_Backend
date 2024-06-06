9f0809f0362a44039b27c05dea73bbdfcfb2d14a
rhenoresky <rheno2002@gmail.com>
2024-06-06 22:47:28 +0700

    add test helper

---
+++ src/routes/account/test/helper/acountHelper.js
@@ -0,0 +1,32 @@
+import { prisma } from "../../../../db/prisma";
+import { passwordHashing } from "../../../../tools/hash";
+
+const deleteAccountsHelper = async () => await prisma.account.deleteMany();
+const getAccountByEmailHelper = async (email = "user1@gmail.com") => {
+  return await prisma.account.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      email: true,
+    },
+  });
+};
+
+const addAccountHelper = async ({
+  email = "user1@gmail.com",
+  password = "user1",
+}) => {
+  const id = Date.now().toString();
+  const passwordHash = await passwordHashing(password);
+
+  await prisma.account.create({
+    data: {
+      id,
+      email,
+      password: passwordHash,
+    },
+  });
+};
+
+export { deleteAccountsHelper, getAccountByEmailHelper, addAccountHelper };
