bab8a732261c4b93f9b438d09a6a5e69f27c8b75
rhenoresky <rheno2002@gmail.com>
2024-07-05 21:24:25 +0700

    storage service

---
+++ src/service/storage/index.js
@@ -0,0 +1,18 @@
+class Storage {
+  constructor(folder) {
+    this._folder = folder;
+
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder, { recursive: true });
+    }
+  }
+
+  async readFIle(file) {
+    const fileName = `${Date.now()}-${file.name}`;
+    await Bun.write(`${this._folder}/${fileName}`, file);
+
+    return `${process.env.HOST}/poster/${fileName}`;
+  }
+}
+
+export default Storage;
