0aa91dcb04728b134891a1fca2a139bfddb5df21
rhenoresky <rheno2002@gmail.com>
2024-07-13 22:15:06 +0700

    refactor var to object

--- src/routes/account/dto.js
+++ src/routes/account/dto.js
@@ -1,21 +1,21 @@
 import { t } from "elysia";
 
-const signUpBodyDto = t.Object({
-  email: t.String({ format: "email" }),
-  password: t.String(),
-  fullname: t.Optional(t.String()),
-});
+export const accountDto = {
+  signUpBodyDto: t.Object({
+    email: t.String({ format: "email" }),
+    password: t.String(),
+    fullname: t.Optional(t.String()),
+  }),
 
-const signInBodyDto = t.Object({
-  email: t.String({ format: "email" }),
-  password: t.String(),
-});
+  signInBodyDto: t.Object({
+    email: t.String({ format: "email" }),
+    password: t.String(),
+  }),
 
-const updateAccountDto = t.Object({
-  email: t.Optional(t.String({ format: "email" })),
-  password: t.Optional(t.String()),
-  fullname: t.Optional(t.String()),
-  role: t.Optional(t.Union([t.Literal("ADMIN"), t.Literal("USER")])),
-});
-
-export { signInBodyDto, signUpBodyDto, updateAccountDto };
+  updateAccountDto: t.Object({
+    email: t.Optional(t.String({ format: "email" })),
+    password: t.Optional(t.String()),
+    fullname: t.Optional(t.String()),
+    role: t.Optional(t.Union([t.Literal("ADMIN"), t.Literal("USER")])),
+  }),
+};
