41e1341eb91c488963604f40602cef548012f82b
rhenoresky <rheno2002@gmail.com>
2024-07-15 18:53:28 +0700

    seed admin

--- prisma/seed/createAdmin.js
+++ prisma/seed/createAdmin.js
@@ -1,6 +1,6 @@
 import { prisma } from "../../src/db/prisma";
 import { generateId } from "../../src/tools/generateId";
-import { passwordHashing } from "../../src/tools/hash";
+import { passwordHashing } from "../../src/utils/hash";
 
 const createAccount = async (password, id) => {
   await prisma.account.create({
