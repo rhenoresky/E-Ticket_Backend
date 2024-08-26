2349d7e21bf7f34c5af0f4456464505db3d7613f
rhenoresky <rheno2002@gmail.com>
2024-08-26 20:46:00 +0700

    change route name

--- src/routes/transaction/controller.js
+++ src/routes/transaction/controller.js
@@ -13,56 +13,6 @@ const midtrans = new Midtrans.Snap({
 const payment = new PaymentMidtrans(midtrans);
 const ticketHandler = new TicketHandler(prisma);
 
-const param = {
-  transaction_details: {
-    order_id: "CustOrder-102",
-    gross_amount: 13000,
-  },
-  credit_card: {
-    secure: true,
-  },
-  item_details: [
-    {
-      id: "a01",
-      price: 7000,
-      quantity: 1,
-      name: "Apple",
-    },
-    {
-      id: "b02",
-      price: 3000,
-      quantity: 2,
-      name: "Orange",
-    },
-  ],
-  customer_details: {
-    first_name: "Budi",
-    last_name: "Susanto",
-    email: "budisusanto@example.com",
-    phone: "+628123456789",
-    billing_address: {
-      first_name: "Budi",
-      last_name: "Susanto",
-      email: "budisusanto@example.com",
-      phone: "08123456789",
-      address: "Sudirman No.12",
-      city: "Jakarta",
-      postal_code: "12190",
-      country_code: "IDN",
-    },
-    shipping_address: {
-      first_name: "Budi",
-      last_name: "Susanto",
-      email: "budisusanto@example.com",
-      phone: "0812345678910",
-      address: "Sudirman",
-      city: "Jakarta",
-      postal_code: "12190",
-      country_code: "IDN",
-    },
-  },
-};
-
 export const ticketController = new Elysia({ prefix: "/ticket" })
   .post(
     "/transaction",
